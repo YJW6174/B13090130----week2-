7a6ed1e0c8af784e9e11f9693772249e6917e396
袁俊文 <yuanjunwen@yuanjunwendeMacBook-Air.local>
2015-03-12 16:44:47 +0800

    Initial Commit

---
+++ homewok3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  homewok3
+//
+//  Created by 袁俊文 on 15/3/12.
+//  Copyright (c) 2015年 袁俊文. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
